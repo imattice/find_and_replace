0e3206a92a193d2c1f726d13faa188931e34cb0d
Ike <gogreen37@me.com>
2015-09-11 11:44:19 -0700

    remove unneccessary complications and restart.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,51 @@
 var superFind = function(word, passage) {
     var passage_no_punctuation = passage.replace(/[.,-\/#!$\^&\*;:{}=\-\_`~()]/g, '');
-    var lower_passage =passage_no_punctuation.toLowerCase();
-    var passage_array = lower_passage.split(' ');
+    // var original_array = passage_no_punctuation.split(' ')
+    var lower_passage = passage_no_punctuation.toLowerCase();
+    var find_array = lower_passage.split(' ');
     var result = [];
 
-    for (var i = 0; i <=passage_array.length; i++) {
-        if (word == passage_array[i]) {
+    for (var i = 0; i <=find_array.length; i++) {
+        if (word.toLowerCase() == find_array[i]) {
+            // for (var = 0; i <=original_array.length; i++)
+            //     if {original_array[i][0]
+            //
+            //     }
+            //     else {
+            //
+            //     }
+            // console.log(find_array[i][0])
             result.push(word);
         }
     }
     return result;
 };
 
-
+//
 var findReplace = function(word, replacement, passage) {
+    // var lower_word = word.toLowerCase();
+    // var found_words = superFind(word, passage);
+    // //console.log(found_words);
+    //
+    // for(var i = 0; i<=found_words.length; i++) {
+    //     if (word.toLowerCase() === found_words[i]) {
+    //         passage.replace(replacement);
+    //     }
+    //
+    //
+    // var new_passage = passage.replace(word, replacement);
+    // return new_passage;
 
-    var new_passage = passage.replace(word, replacement)
+    if (word === passage[i])
+        passage.replace(passage[i], replacement)
 
-    return new_passage;
 }
+
+
+//capitalization ideas
+
+    //do not lowercase the find array and push the original found letter into the result.  Then, in findReplace, check if the letter[0] in the find array is uppercase.  If it is, uppercase the replacement[0].
+        // find_array[i].toLowerCase() does not work.  Cannot call .toLowerCase on undefined
+
+    //two arrays and compare at index number
+        //again, cannot call toUpperCase() on a looped array item
--- spec/specs.js
+++ spec/specs.js
@@ -1,30 +1,46 @@
-describe('superFind', function() {
-
-    it('finds a word within a given passage', function() {
-        expect(superFind('hello', 'hello my name is ike')).to.eql(['hello'])
-    });
-
-    it('finds multiple words within a passage', function() {
-        expect(superFind('cat', 'I live with one cat.  The cat belongs to my roommate. The cat is brown and has long cat hair.')).to.eql(['cat', 'cat', 'cat', 'cat'])
-    });
-
-    it('finds word regardless of captialization', function() {
-        expect(superFind('carbuncle', 'My roommate\'s cAt is named Carbuncle. carbuncle really likes to chase lasors.')).to.eql(['carbuncle', 'carbuncle'])
-    });
-})
-
 describe('findReplace', function() {
+    it('finds a word within a given passage', function() {
+        expect(findReplace('hello', 'hello my name is ike')).to.eql(['hello'])
+    });
 
     it('replaces found word within a given passage', function() {
         expect(findReplace('plants', 'feet', 'Sometimes she will attack our plants.')).to.equal('Sometimes she will attack our feet.');
     });
-    //
-    // it('maintains captialization if the input is not capitalized', function() {
-    //     expect(findReplace('she', 'carbuncle', 'She meows when she is locked out of any room.')).to.equal('Carbuncle meows when she is locked out of any room.')
-    // });
-    //
-    // it('replaces all words within a given passage', function() {
-    //     expect(findReplace('good', 'great','She is still a good cat, though.  Good cats can be hard to find.')).to.equal('She is still a great cat, though.  Great cats can be hard to find.')
-    // });
+}
 
-})
+
+
+
+
+//tests for unneccessary complications
+    // describe('superFind', function() {
+    //
+    //     it('finds a word within a given passage', function() {
+    //         expect(superFind('hello', 'hello my name is ike')).to.eql(['hello'])
+    //     });
+    //
+    //     it('finds multiple words within a passage', function() {
+    //         expect(superFind('cat', 'I live with one cat.  The cat belongs to my roommate. The cat is brown and has long cat hair.')).to.eql(['cat', 'cat', 'cat', 'cat'])
+    //     });
+    //
+    //     it('finds word regardless of captialization', function() {
+    //         expect(superFind('carbuncle', 'My roommate\'s cAt is named Carbuncle. carbuncle really likes to chase lasors.')).to.eql(['carbuncle', 'carbuncle'])
+    //     });
+    // })
+    //
+    // //split into two functions because two differnt output types
+    // describe('findReplace', function() {
+    //
+    //     it('replaces found word within a given passage', function() {
+    //         expect(findReplace('plants', 'feet', 'Sometimes she will attack our plants. plants.  plants.  plants.')).to.equal('Sometimes she will attack our feet.  feet.  feet.  feet.');
+    //     });
+
+        // it('maintains captialization if the input is not capitalized', function() {
+        //     expect(findReplace('she', 'carbuncle', 'She meows when she is locked out of any room.')).to.equal('Carbuncle meows when she is locked out of any room.')
+        // });
+        // //
+        // it('replaces all words within a given passage', function() {
+        //     expect(findReplace('good', 'great','She is still a good cat, though.  Good cats can be hard to find.')).to.equal('She is still a great cat, though.  Great cats can be hard to find.')
+        // });
+
+    // })
